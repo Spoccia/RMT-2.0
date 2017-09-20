88c3535f48f619c90c3f1560c97b835f109a0609
Spoccia <silvestro.poccia@gmail.com>
2017-09-20 15:12:26 -0700

    update

--- trainFeaturesScripts/SVM_Training.m
+++ trainFeaturesScripts/SVM_Training.m
@@ -1,14 +1,37 @@
 clear;
 clc;
 
+%% Input variable
+% featureFolder = Folder that contains the features
+% dataFolder =  Folder that contains the data
+% destFolder = Destination where to save trained SVM model
+% 
+% SelectDataset = 1 full scale  change the descriptor start end position
+%                 2 for all the others
+% startDescr = location where the descriptor start
+% endDescr   = location where the descriptor ends
+% dataSize   = number of timeseries in the dataset 
+% trainingPercentage = what percentage of data we should use to train the SVM
+% ClassIndexes Sart and end index for eleents of each class
+
 
 featureFolder = ['D:\Test Traning\dataFeatures'];
 dataFolder = ['D:\Test Traning\data'];
 destFolder = ['D:\Test Traning\dataFeatures\SVMTrained\'];
 
-saveFolder = '.';
+SelectDataset= 1; % 1 means full scale ; 2 means hybrid or fix scale
+startDescr =8;
+endDescr = 135;
+%saveFolder = '.';
 dataSize = 184;
-trainingPercentage = 0.6;
+trainingPercentage = 0.6; 
+% OBSERVATION: take 60% require to do multiple test  using the remaning 40%  to understand what is the best set to use for the query.
+
+
+if(SelectDataset ==1)
+    startDescr =11;
+    endDescr = 138;
+end
 
 % MoCap data Class
 ClassIndexes = [1, 15, 51, 81, 99, 118, 149, 179, 185];
@@ -65,7 +88,11 @@ for queryID =1:184
                 index_Of_Octave= (AllFeaturesClass(5,:)==OD & AllFeaturesClass(6,:)==OT )
                 OctaveClassification=ClassificationIDX(index_Of_Octave,1);
                 OctaveFeatures = AllFeaturesClass(:,index_Of_Octave);
-                Mdl = fitcecoc(OctaveFeatures(8:135,:)',OctaveClassification)
+                % Xilun Suggested function
+                Mdl = fitcecoc(OctaveFeatures(startDescr:endDescr,:)',OctaveClassification);
+                % From my reading  this should be fine
+                SVMModel = fitcsvm(X,Y,'KernelFunction','linear','Standardize',true,'ClassNames',{'1','2'});
+                
                 if(exist(strcat(destFolder,num2str(queryID),'\'),'dir')==0)
                     mkdir(strcat(destFolder,num2str(queryID),'\'));
                 end
@@ -76,6 +103,3 @@ for queryID =1:184
         
     end
 end
-
-
-'end'
