6b4fb4381585b12bea083378f62888d3340e8ae4
Sicong Liu <sicongliu@Sicongs-MacBook-Pro.local>
2017-12-01 21:21:59 -0700

    Finished allignCLusterFeatures.m, delivered to Silvestro for Checking

--- RMT-2.0_Motif/utility/allignCLusterFeatures.m
+++ RMT-2.0_Motif/utility/allignCLusterFeatures.m
@@ -26,6 +26,9 @@ load(savepath1);
 load(savepath2);
 load(savepath3);
 depdOverLapThreshold = 0.5;
+
+reOrgFeatures = [];
+subFeatureLabels = []; % design as a row vector
 for timeOctave = 2:DeOctTime
     for depdOctave = 2:DeOctDepd
         if(exist(strcat(ClusterPath, '\Cluster_IM_', imagename, '_DepO_', num2str(depdOctave), '_TimeO_', num2str(timeOctave), '.csv'), 'file')~=0)
@@ -40,6 +43,11 @@ for timeOctave = 2:DeOctTime
             mu = csvread(strcat(ClusterPath,'\Centroids_IM_',imagename,'_DepO_',num2str(depdOctave),'_TimeO_',num2str(timeOctave),'.csv'));
             clusterLabel = unique(C);
             numberOfClusters = length(clusterLabel);
+            
+            % subCluster label for interesting octave monotonically increases
+            % for features from different octaves, subClusterLabel reset to 1
+            subClusterLabel = 1; 
+            
             for clusterIndex = 1 : numberOfClusters
                 % get the features from current clutser index
                 clusterFeatures = X(:, C == clusterLabel(clusterIndex));
@@ -48,44 +56,63 @@ for timeOctave = 2:DeOctTime
                 depdScale = depdScaleRead(:, C == clusterLabel(clusterIndex));
                 
                 % should use cell structure to index cluster features
-                numberOfClusterFeatures = size(clusterFeatures, 2);
-                Depd_OverLapping = cell(numberOfClusterFeatures, 1);
+                Depd_OverLapping = zeros(size(clusterFeatures, 2));
+                
                 for queryFeatureIndex = 1 : size(clusterFeatures, 2)
                     % dataFeatureIndex starts from queryFeatureIndex since our goal is to group feature from the same simulation data
-                    for dataFeatureIndex = 1 : size(clusterFeatures, 2)
+                    for dataFeatureIndex = queryFeatureIndex : size(clusterFeatures, 2)
                         % use symmetric approach to define the similiar motifs below
                         overlap = overlapJaccardSimilarity(depdScale(:, queryFeautreIndex), depdScale(:, dataFeatureIndex));
-                        if(overlap > depdOverLapThreshold)
-                            Depd_OverLapping{queryFeatureIndex} = [Depd_OverLapping{queryFeatureIndex}, dataFeatureIndex];
-                            % since its symmetric, no need to mark dataFeatureIndex
-                        end
+                        Depd_OverLapping(queryFeatureIndex, dataFeatureIndex) = overlap;
+                        Depd_OverLapping(dataFeatureIndex, queryFeatureIndex) = overlap;
                     end
                 end
-                % get rid of cell that contains only one data element
-                Depd_OverLapping = Depd_OverLapping(~cellfun(@(x) size(x, 2)==1, Depd_OverLapping));
-                % to check feature subcluster, under clusterIndex->Depd_OverLapping cell index
+                Depd_OverLapping = Depd_OverLapping > depdOverLapThreshold;
+                stopFlag= 0;
+                while(stopFlag == 0 )
+                    columnSum = sum(Depd_OverLapping); % row vector
+                    [maxSum, index] = max(columnSum);
+                    if(maxSum == 0) % check maxSum condition at the beginning
+                        stopFlag = 1;
+                    else
+                        subClusterFeatureIndex = Depd_OverLapping(:, index); % column vector
+                        nonZeroEntryIndex = find(subClusterFeatureIndex ~= 0); % nonZeroEntryIndex is the actual feature index from current cluster
+                        subClusterFeatures =  clusterFeatures(:, subClusterFeatureIndex);
+                        
+                        % re-org features and the feathre labels
+                        reOrgFeatures = [reOrgFeatures, subClusterFeatures];
+                        currentFeatureLabel = [currentFeatureLabel, repmat(subClusterLabel, 1, size(nonZeroEntryIndex, 1))]; % design as a row vector
+                        
+                        % save as index: subClusterLabel
+                        subClusterLabel = subClusterLabel + 1;
+                        
+                        % now update Depd_OverLapping matrix
+                        zeroColumn = zeros(size(subClusterFeatureIndex));
+                        Depd_OverLapping(:, index) = zeroColumn;
+                    end
+                end
                 
-                
-%                 %%  need to revise the apporach below
-%                 % timeScope = A(4, :)*3;
-%                 Depd_OverLapping = zeros(size(clusterFeatures, 2));
-%                 for queryFeatureIndex = 1 : size(clusterFeatures, 2)
-%                     % dataFeatureIndex starts from queryFeatureIndex since our goal is to group feature from the same simulation data
-%                     % for dataFeatureIndex = queryFeatureIndex : size(A, 2)
-%                     for dataFeatureIndex = 1 : size(clusterFeatures, 2)
-%                         % Depd_OverLapping(queryFeatureIndex, dataFeatureIndex) = (size(intersect(B(:,queryFeatureIndex),B(:,dataFeatureIndex)),1)-1)/(size(union(B(:, queryFeatureIndex), B(:,dataFeatureIndex)), 1)-1);
-%                         % Depd_OverLapping(dataFeatureIndex, queryFeatureIndex) = Depd_OverLapping(queryFeatureIndex,dataFeatureIndex);
-%                         
-%                         % use separate functions below, as shown in RMT journal paper, the values should not be symetric
-%                         Depd_OverLapping(queryFeatureIndex, dataFeatureIndex) = overlapQueryFeature(depdScale(:, queryFeautreIndex), depdScale(:, dataFeatureIndex));
-%                     end
-%                 end
-%                 idx_Depd_OverLapping = Depd_OverLapping > depdOverLapThreshold;
-%                 idxmax = max(sum(idx_Depd_OverLapping));
-%                 A1 = clusterFeatures(:, idx_Depd_OverLapping(idxmax, :));
-%                 B1 = depdScale(:, idx_Depd_OverLapping(idxmax, :));
-%                 % timeScope1 = A1(4, :)*3;
-            end
+                % Depd_OverLapping = Depd_OverLapping(~cellfun(@(x) size(x, 2)==1, Depd_OverLapping));
+                %                 %%  need to revise the apporach below
+                %                 % timeScope = A(4, :)*3;
+                %                 Depd_OverLapping = zeros(size(clusterFeatures, 2));
+                %                 for queryFeatureIndex = 1 : size(clusterFeatures, 2)
+                %                     % dataFeatureIndex starts from queryFeatureIndex since our goal is to group feature from the same simulation data
+                %                     % for dataFeatureIndex = queryFeatureIndex : size(A, 2)
+                %                     for dataFeatureIndex = 1 : size(clusterFeatures, 2)
+                %                         % Depd_OverLapping(queryFeatureIndex, dataFeatureIndex) = (size(intersect(B(:,queryFeatureIndex),B(:,dataFeatureIndex)),1)-1)/(size(union(B(:, queryFeatureIndex), B(:,dataFeatureIndex)), 1)-1);
+                %                         % Depd_OverLapping(dataFeatureIndex, queryFeatureIndex) = Depd_OverLapping(queryFeatureIndex,dataFeatureIndex);
+                %
+                %                         % use separate functions below, as shown in RMT journal paper, the values should not be symetric
+                %                         Depd_OverLapping(queryFeatureIndex, dataFeatureIndex) = overlapQueryFeature(depdScale(:, queryFeautreIndex), depdScale(:, dataFeatureIndex));
+                %                     end
+                %                 end
+                %                 idx_Depd_OverLapping = Depd_OverLapping > depdOverLapThreshold;
+                %                 idxmax = max(sum(idx_Depd_OverLapping));
+                %                 A1 = clusterFeatures(:, idx_Depd_OverLapping(idxmax, :));
+                %                 B1 = depdScale(:, idx_Depd_OverLapping(idxmax, :));
+                %                 % timeScope1 = A1(4, :)*3;
+            end % end marking current cluster at interesting octave
         end
     end
 end
