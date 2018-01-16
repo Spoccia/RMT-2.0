c1e7b2ed9c6bc58459d571dc78787dbc7f1ad4f6
Sicong Liu <sicongliu@Sicongs-MacBook-Pro.local>
2018-01-16 09:10:07 -0700

    Dummy Update

--- RMT-2.0_Motif/CodeAccuracy/new  code/PrecisionRecall.m
+++ RMT-2.0_Motif/CodeAccuracy/new  code/PrecisionRecall.m
@@ -1,5 +1,10 @@
 clear;
 clc;
+
+% load feature count into motifFeatureCount
+% motifFeatureCount
+
+
 % [num,txt,raw] = xlsread('D:\Motif_Results\Datasets\SynteticDataset\Features_RME\Mocap_test6_AllFeatures\Accuracy\AP_Mocap_test6_AllFeatureFound_DepO_2_DepT_2.csv','AP_all_SubC');
 filePath = ['./AP_Mocap_test6_AllFeatureFound_DepO_2_DepT_2.csv'];
 [num,txt,raw] = xlsread(filePath,'AP_all_SubC');
@@ -14,9 +19,7 @@ injectedTimeEnd = num(:, 7);
 
 % depd overlap computed from Jaccard similarity
 depdOverlap = num(:, 8);
-isQualified = num(:, 9);
-
-timeOverlap = computeTimeOverlap(timeStart, timeEnd, injectedTimeStart, injectedTimeEnd);
+timeOverlap = num(:, 9);
 
 timeOverlapThreshold = 0.5;
 depdOverlapThreshold = 0.5;
@@ -26,52 +29,71 @@ recall = [];
 FScore = [];
 
 uniqueInjectedClass = unique(injectedClassID);
-
 % for each of the injected class, we have a precision and recall
-for i = 1 : uniqueInjectedClass
+for i = 1 : size(uniqueInjectedClass, 1)
+    currentInjectedClassIndex = uniqueInjectedClass(i);
+    % relevantFeatureCount used to compute recall
+    relevantFeatureCount = size(find(motifFeatureCount == i), 1);
     
+    % currentFeatureCount used to compute precision
+    retrievedIndex = injectedClassID == currentInjectedClassIndex;
     
+    currentFeatureCount = size(retrievedIndex~=0, 1);
+    % filter with threshold
+    preFilteredTimeOverlap = timeOverlap(retrievedIndex);
+    preFilteredDepdOverlap = depdOverlap(retrievedIndex);
     
-    precision = [precision, currentBestPrecision];
-    recall = [recall, currentBestRecall];
-    FScore = [FScore, currentBestPrecision];
+    filteredTimeOverlapIndex = preFilteredTimeOverlap > timeOverlapThreshold;
+    preFilteredDepdOverlap = preFilteredDepdOverlap(filteredTimeOverlapIndex);
+    filteredDepdOverlap = filteredDepdOverlap(preFilteredDepdOverlap > depdOverlapThreshold);
+    
+    relevantSize = size(filteredDepdOverlap, 1);
+    currentPrecision = relevantSize/currentFeatureCount;
+    currentRecall = relevantSize/relevantFeatureCount;
+    
+    currentFScore = 2 * currentPrecision * currentRecall / (currentRecall + currentPrecision);
+    
+    precision = [precision, currentPrecision];
+    recall = [recall, currentRecall];
+    FScore = [FScore, currentFScore];
 end
 
-currentClass = unique(classID);
-for i = 1 : currentClass
-    index = find(classID == i);
-    currentDataEntry = num(index, :);
-    
-    currentInjectedClassID = injectedClassID(index, :);
-    currentDepdOverlap = depdOverlap(index, :);
-    currentIsQualified = isQualified(index, :);
-    currentTimeOverlap = timeOverlap(index, :);
-    
-    % for each currentInjectedClassID, use voting scheme to pick the best one for representation
-    currentUniqueClass = unique(currentInjectedClassID);
-    currentBestFScore = realmin;
-    currentBestPrecision = 0;
-    currentBestRecall = 0;
-    for j = 1 : currentUniqueClass
-        % filter using threshold
-        currentIndex = find(currentInjectedClassID == j);
-        filteredIndex = find(currentDepdOverlap(currentIndex) > depdOverlapThreshold& currentTimeOverlap(currentIndex) > timeOverlapThreshold);
-        % pick the best FScore
-        
-        currentFScore = (2 * currentPrecision * currentRecall) / (currentPrecision + currentRecall);
-        if(currentFScore > currentBestFScore)
-            currentBestPrecision = currentPrecision;
-            currentBestRecall = currentRecall;
-            currentBestFScore = currentFScore;
-        end
-        
-        
-    end
-    
-    precision = [precision, currentBestPrecision];
-    recall = [recall, currentBestRecall];
-    FScore = [FScore, currentBestPrecision];
-end
+% 
+% currentClass = unique(classID);
+% for i = 1 : currentClass
+%     index = find(classID == i);
+%     currentDataEntry = num(index, :);
+%     
+%     currentInjectedClassID = injectedClassID(index, :);
+%     currentDepdOverlap = depdOverlap(index, :);
+%     currentIsQualified = isQualified(index, :);
+%     currentTimeOverlap = timeOverlap(index, :);
+%     
+%     % for each currentInjectedClassID, use voting scheme to pick the best one for representation
+%     currentUniqueClass = unique(currentInjectedClassID);
+%     currentBestFScore = realmin;
+%     currentBestPrecision = 0;
+%     currentBestRecall = 0;
+%     for j = 1 : currentUniqueClass
+%         % filter using threshold
+%         currentIndex = find(currentInjectedClassID == j);
+%         filteredIndex = find(currentDepdOverlap(currentIndex) > depdOverlapThreshold& currentTimeOverlap(currentIndex) > timeOverlapThreshold);
+%         % pick the best FScore
+%         
+%         currentFScore = (2 * currentPrecision * currentRecall) / (currentPrecision + currentRecall);
+%         if(currentFScore > currentBestFScore)
+%             currentBestPrecision = currentPrecision;
+%             currentBestRecall = currentRecall;
+%             currentBestFScore = currentFScore;
+%         end
+%         
+%         
+%     end
+%     
+%     precision = [precision, currentBestPrecision];
+%     recall = [recall, currentBestRecall];
+%     FScore = [FScore, currentBestPrecision];
+% end
 averageFScore = mean(FScore);
 
 fprintf('All done .\n');
