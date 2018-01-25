75b13662595c5202490976deeacdadd00d523440
Spoccia <silvestro.poccia@gmail.com>
2018-01-25 11:35:48 -0700

    up

--- RMT-2.0_Motif/CodeAccuracy/new  code/AccuracyStarter.m
+++ RMT-2.0_Motif/CodeAccuracy/new  code/AccuracyStarter.m
@@ -2,7 +2,7 @@ clc; clear;
 
 path='D:\Motif_Results\Datasets\SynteticDataset\';
 kindofinj='data\';%'CosineTS_MultiFeatureDiffClusters\';%'MultiFeatureDiffClusters\';
-for NAME =32:32%1:33%22:33%16:21%:21
+for NAME =46:46%57%1:33%22:33%16:21%:21
  TEST = ['Mocap_test',num2str(NAME)]%'Mocap_test11';
 % TEST=['MoCap',num2str(NAME)]
 FeaturesRM ='RMT';%'RME';%
@@ -28,11 +28,11 @@ for DepO =2:2
 %         AP_MAX_OverlappingInTime(path,kindofinj,TEST,FeaturesRM,kindofCluster,measure,ClusterAlg,subfolderClusterLabel,num2str(DepO),num2str(DepT));
 %         AP_MAX_OverlappingInTime_subcluster(path,kindofinj,TEST,FeaturesRM,kindofCluster,measure,ClusterAlg,subfolderClusterLabel,num2str(DepO),num2str(DepT));
         
-        for AfterPruning =0:1
-            for subcluster=0:1
-                Distortion_Clusters(path,kindofinj,TEST,FeaturesRM,kindofCluster,measure,ClusterAlg,subfolderClusterLabel,num2str(DepO),num2str(DepT),AfterPruning,subcluster);
-            end
-        end
+%         for AfterPruning =0:1
+%             for subcluster=0:1
+%                 Distortion_Clusters(path,kindofinj,TEST,FeaturesRM,kindofCluster,measure,ClusterAlg,subfolderClusterLabel,num2str(DepO),num2str(DepT),AfterPruning,subcluster);
+%             end
+%         end
         
     end
 end
